9145fba5393d229ee6c2bb692d27ad98f681932c
JoCatherine Griggs <jocafrin@gmail.com>
2018-09-07 15:01:35 -0400

    endpoints created

--- server.js
+++ server.js
@@ -7,3 +7,13 @@ server.use(express.json());
 
 // ___endpoints__
 
+server.get('/projects', (req, res) => {
+    projects.get()
+    .then(projects => {
+        res.status(200).json(projects);
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: "The project information could not be retrieved." });
+    })
+})
